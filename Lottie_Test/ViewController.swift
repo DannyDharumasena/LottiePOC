e405e808b5d41b5e89d2e4ee451e31eb5b35a628
Danny Dharumasena <danny@robs-macbook-pro.local>
2017-10-10 16:39:01 +1100

    Lottie POC for button animations

--- Lottie_Test/ViewController.swift
+++ Lottie_Test/ViewController.swift
@@ -6,20 +6,36 @@
 //  Copyright © 2017 Danny Dharumasena. All rights reserved.
 //
 
-import UIKit
+import UIKit  
+import Lottie
 
 class ViewController: UIViewController {
+    var animationView: LOTAnimationView = LOTAnimationView(name: "icon_Uploadtick")
+    
+    var animationView2: LOTAnimationView = LOTAnimationView(name: "btn_Green_Shk_TextLottie1")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        animationView = LOTAnimationView(name: "icon_Uploadtick")
+        animationView.center = self.view.center
+        animationView.frame.origin.y -= 100
+        
+        self.view.addSubview(animationView)
+        
+        animationView.play()
+        
+       // animationView2 = LOTAnimationView(name: "btn_Green_Shk_Lottie13")
+        //animationView2.center = self.view.center
+       // animationView2.frame.origin.y -= 200
+        
+       // self.view.addSubview(animationView2)
+        
+       // animationView2.play()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    @IBAction func playAnimation(_ sender: UIButton)
+ {
 }
-
+}
